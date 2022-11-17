f40ae96fd0192cb60b73409d1ff8e1889a94948c
Araj00 <aman02437@gmail.com>
2022-11-17 10:56:37 +0530

    signup route changed and test file added

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import About from './pages/About';
+import '../src/pages/style.css'
+import { Home } from './pages/Home';
+import Login from './pages/Login';
+import Loginhome from './pages/Loginhome';
+import {  Route,Routes } from 'react-router-dom';
+import Nav from './componants/Nav';
+import Test from './componants/Test';
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+  
+      <Route path='/' element={<Nav/>}>
+
+<Route path='/home' element={<Home/>} />
+<Route path='/About' element={<About/>} />
+<Route path='/sigup' element={<Login/>} />
+<Route path='/signin' element={<Loginhome/>} />
+<Route path='/test' element = {<Test/>}/>
+  </Route>
+</Routes>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/Test/Test.jsx
@@ -0,0 +1,17 @@
+import React  from 'react'
+// import Slide from '../../componants/Slide'
+import { Testt } from '../../componants/Testt'
+import { Nav } from '../../componants/Nav'
+import './test.css'
+
+const Test = () => {
+
+  return (
+    <div>
+      <Nav/>
+      <Testt/>
+    </div>
+  )
+}
+
+export default Test
